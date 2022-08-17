cd424372e27089dbd4b2e570e6efbaa7cdd73ec8
g-boram <go_bo_ram@naver.com>
2022-08-17 17:03:40 +0900

    리액트 200 진도 완료

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Route } from "react-router-dom";
+import cookie from 'react-cookies';
+import axios from "axios";
+
 
 // css
 import '../css/new.css';
-
 // header
 import HeaderAdmin from './Header/Header admin';
-
 // footer
 import Footer from './Footer/Footer';
-
 // Login
 import LoginForm from './LoginForm';
 // ReactProxy
@@ -20,17 +20,66 @@ import ApiPostJson from './ApiPostJson';
 import SoftwareList from './SoftwareToolsManage/SoftwareList';
 // SoftwareView
 import SoftwareView from './SoftwareToolsManage/SoftwareView';
+// Register
+import Register from './Register/Register';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  componentDidMount() {
+    axios.post('/api/LoginForm?type=SessionConfirm', {
+      token1 : cookie.load('userid') 
+      , token2 : cookie.load('username') 
+    })
+    .then( response => {
+        this.state.userid = response.data.token1
+        let password = cookie.load('userpassword')
+        if(password !== undefined){
+          axios.post('/api/LoginForm?type=SessionSignin', {
+            is_Email: this.state.userid,
+            is_Token : password
+          })
+          .then( response => {
+            if(response.data.json[0].useremail === undefined){
+              this.noPermission()
+            }
+          })
+          .catch( error => {
+            this.noPermission()
+          });
+        }else{
+          this.noPermission()
+        }
+    })
+    .catch( response => this.noPermission());
+  }
+
+  noPermission = (e) => {
+    if(window.location.hash != 'nocookie'){
+      this.remove_cookie();
+      window.location.href = '/login/#nocookie';
+    }
+  };
+
+  remove_cookie = (e) => {
+    cookie.remove('userid', { path: '/'});
+    cookie.remove('username', { path: '/'});
+    cookie.remove('userpassword', { path: '/'});
+  }
+
   render () {
     return (
       <div className="App">
         <HeaderAdmin/> 
         <Route exact path='/' component={LoginForm} />
-        <Route exact path='/SoftwareList' component={SoftwareList} />
-        <Route exact path='/SoftwareView/:swtcode' component={SoftwareView} />
-        <Route exact path='/reactProxy' component={ReactProxy} />
-        <Route exact path='/apiPostJson' component={ApiPostJson} />
+        <Route path='/login' component={LoginForm} />
+        <Route path='/SoftwareList' component={SoftwareList} />
+        <Route path='/SoftwareView/:swtcode' component={SoftwareView} />
+        <Route path='/register' component={Register} />
         <Footer/>
       </div>
     );
